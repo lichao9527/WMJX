551bf1b61a79f2df1e507248dee37d6d6149865b
dc003 <dc003@lichao.local>
2016-06-22 18:45:40 +0800

    添加微文明

---
+++ 文明嘉兴/BlogViewController.h
@@ -0,0 +1,13 @@
+//
+//  BlogViewController.h
+//  文明嘉兴
+//
+//  Created by dc003 on 16/6/22.
+//  Copyright © 2016年 dc003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlogViewController : UIViewController
+
+@end
---
+++ 文明嘉兴/BlogViewController.m
@@ -0,0 +1,40 @@
+//
+//  BlogViewController.m
+//  文明嘉兴
+//
+//  Created by dc003 on 16/6/22.
+//  Copyright © 2016年 dc003. All rights reserved.
+//
+
+#import "BlogViewController.h"
+
+@interface BlogViewController ()
+
+@end
+
+@implementation BlogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIWebView *webView=[[UIWebView alloc]initWithFrame:self.view.bounds];
+    [self.view addSubview:webView];
+    NSURLRequest *request=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://weibo.com/u/2852517300?is_hot=1"]];
+    [webView loadRequest:request];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
